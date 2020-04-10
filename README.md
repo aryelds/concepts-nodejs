058860bcbb3af3c785936fb31fb00d3571e25b25
Aryel Santos <aryel.ds@gmail.com>
2020-04-10 13:29:42 -0300

    Add final files

---
+++ README.md
@@ -0,0 +1,35 @@
+Concepts nodeJs
+=================
+
+Simple application using [NodeJS](https://nodejs.org/en), [Express](https://expressjs.com/) and [Jest](https://jestjs.io/).
+ 
+## How to run
+
+```
+$ git clone https://github.com/aryelds/concepts-nodejs.git
+```
+
+Install the dependencies
+
+```
+$  yarn install
+```
+
+Run the application
+
+```
+$  yarn dev
+```
+
+Run tests
+
+```
+$  yarn dev
+```
+
+Check in the browser
+
+[http://localhost:3333/repositories](http://localhost:3333/repositories)
+
+
+
--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,76 @@ app.use(cors());
 
 const repositories = [];
 
+function validateID(request, response, next) {
+    const { id } = request.params;
+
+   if(!isUuid(id)) {
+       return response.status(400).json({"error": "Invalid ID!"});
+   }
+
+    return  next();
+}
+
+app.use('/repositories/:id', validateID);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+    return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+    const { title, url, techs } = request.body;
+    const likes = 0;
+    const repository = {id: uuid(), title, url, techs, likes};
+
+    repositories.push(repository);
+
+    return response.status(200).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+    const { id } = request.params;
+    const { title, url, techs } = request.body;
+    const index = repositories.findIndex((repo) => repo.id === id);
+
+    if (index < 0) {
+        return response.status(400).send('ID not found');
+    }
+
+    const likes = repositories[index]['likes'];
+    const repository = { id,title, url, techs, likes};
+
+    repositories[index] = repository;
+
+    return response.json(repository);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+    const { id } = request.params;
+    const index = repositories.findIndex((repo) => repo.id === id);
+
+    if (index < 0) {
+        return response.status(400).send('ID not found');
+    }
+
+    repositories.splice(index, 1);
+
+    return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateID, (request, response) => {
+    const { id } = request.params;
+    const index = repositories.findIndex((repo) => repo.id === id);
+
+    if (index < 0) {
+        return response.status(400).send('ID not found');
+    }
+
+    let likes = repositories[index].likes;
+    likes++;
+
+    repositories[index]["likes"] = likes;
+
+    response.status(200).json(repositories[index]);
 });
 
 module.exports = app;
